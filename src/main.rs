9d95ab8970120bf8f84347bf72d540edfbcfcfe0
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2024-12-29 20:49:43 +0100

    feat: adding tests

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,13 @@ fn main() {
     println!("Hello, world!");
     println!("I'm a Rustacean!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        main();
+    }
+}
